8d74b43c46771a9cf9c40a434840a332c118844e
Greg Wilwerding <greg.wilwerding@gmail.com>
2017-11-29 17:37:25 -0600

    links

--- JawJacker_rails/test/controllers/static_pages_controller_test.rb
+++ JawJacker_rails/test/controllers/static_pages_controller_test.rb
@@ -1,27 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "JawJacker"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    gget help_path
     assert_response :success
-    assert_select "title", "JawJacker"
+    assert_select "title", "Help | JawJacker"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "JawJacker"
+    assert_select "title", "About | JawJacker"
   end
 
   test "should get contact" do
-    get static_pages_about_url
+    get contact_path
     assert_response :success
-    assert_select "title", "JawJacker"
+    assert_select "title", "Contact | JawJacker"
   end
 end
