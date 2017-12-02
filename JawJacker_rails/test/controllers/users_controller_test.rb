0f187ac6b7c35238061203bb67f6924e42607a47
Greg Wilwerding <greg.wilwerding@gmail.com>
2017-12-02 15:10:22 -0600

    show all users

--- JawJacker_rails/test/controllers/users_controller_test.rb
+++ JawJacker_rails/test/controllers/users_controller_test.rb
@@ -18,10 +18,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-  test "should redirect update when not logged in" do
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
-    assert_not flash.empty?
+  test "should redirect index when not logged in" do
+    get users_path
     assert_redirected_to login_url
   end
 
